c154e93cad927f5e21a95e9448f017bfcf4e82cc
W1llbedone <n.tipper05@gmail.com>
2025-03-17 12:37:07 -0700

    added no opt to compile flags

--- build.bat
+++ build.bat
@@ -1,1 +1,12 @@
-cl /I .\include\ .\src\main.cpp .\src\glad.c /link glfw3_mt.lib opengl32.lib user32.lib gdi32.lib Shell32.lib /LIBPATH:lib
+@echo off
+
+if exist .\build\ (
+    rmdir /s /q .\build
+)
+
+mkdir .\build
+
+pushd .\build
+cl /I ..\include\ /Od ..\src\main.cpp ..\src\glad.c /link glfw3_mt.lib opengl32.lib user32.lib gdi32.lib Shell32.lib /LIBPATH:..\lib
+del *.obj
+popd
